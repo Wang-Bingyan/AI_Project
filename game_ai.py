c6bbbc1dbd34a738bebc87e91a6b4cc7bfd2716a
Wang-Bingyan <3170104173@zju.edu.cn>
2020-06-28 19:47:29 +0800

    重构，优化了评估函数“

--- game_ai.py
+++ game_ai.py
@@ -4,17 +4,32 @@ import random
 
 # 评分表
 value_table = [
+
+    # 成五
     [[1, 1, 1, 1, 1], 50001],
     [[-1, -1, -1, -1, -1], -50000],
 
+    # 活四
     [[0, 1, 1, 1, 1, 0], 4321],
     [[0, -1, -1, -1, -1, 0], -4320],
 
-    [[0, 1, 1, 1, 0, 0], 721],
-    [[0, -1, -1, -1, 0, 0], -720],
+    # 冲四
+    [[1, 1, 1, 1, 0], 1441],
+    [[-1, -1, -1, -1, 0], -1440],
+    [[0, 1, 1, 1, 1], 1441],
+    [[0, -1, -1, -1, -1], -1440],
+    [[1, 1, 0, 1, 1], 1441],
+    [[-1, -1, 0, -1, -1], -1440],
+    [[1, 0, 1, 1, 1], 1441],
+    [[-1, 0, -1, -1, -1], -1440],
+    [[1, 1, 1, 0, 1], 1441],
+    [[-1, -1, -1, 0, -1], -1440],
 
-    [[0, 0, 1, 1, 1, 0], 721],
-    [[0, 0, -1, -1, -1, 0], -720],
+    # 活三
+    [[0, 1, 1, 1, 0], 721],
+    [[0, -1, -1, -1, 0], -720],
+    [[0, 1, 1, 1, 0], 721],
+    [[0, -1, -1, -1, 0], -720],
 
     [[0, 1, 1, 0, 1, 0], 721],
     [[0, -1, -1, 0, -1, 0], -720],
@@ -22,21 +37,7 @@ value_table = [
     [[0, 1, 0, 1, 1, 0], 721],
     [[0, -1, 0, -1, -1, 0], -720],
 
-    [[1, 1, 1, 1, 0], 721],
-    [[-1, -1, -1, -1, 0], -720],
-
-    [[0, 1, 1, 1, 1], 721],
-    [[0, -1, -1, -1, -1], -720],
-
-    [[1, 1, 0, 1, 1], 721],
-    [[-1, -1, 0, -1, -1], -720],
-
-    [[1, 0, 1, 1, 1], 721],
-    [[-1, 0, -1, -1, -1], -720],
-
-    [[1, 1, 1, 0, 1], 721],
-    [[-1, -1, -1, 0, -1], -720],
-
+    # 活二
     [[0, 0, 1, 1, 0, 0], 121],
     [[0, 0, -1, -1, 0, 0], -120],
 
@@ -46,61 +47,18 @@ value_table = [
     [[0, 1, 0, 1, 0, 0], 121],
     [[0, -1, 0, -1, 0, 0], -120],
 
-    [[0, 0, 0, 1, 0, 0], 21],
-    [[0, 0, 0, -1, 0, 0], -20],
+    [[0, 0, 1, 0, 0], 21],
+    [[0, 0, -1, 0, 0], -20],
 
-    [[0, 0, 1, 0, 0, 0], 21],
-    [[0, 0, -1, 0, 0, 0], -20],
+    [[0, 0, 1, 0, 0], 21],
+    [[0, 0, -1, 0, 0], -20],
 ]
-
-
-# AI接口，传入一张棋盘，返回下一步的行棋位置，row表示行，column表示列
-def ai_move(board, neighborhood):
-    size = np.size(board, 0)
-    row = size // 2
-    column = size // 2
-    min_value = 114514
-
-    # ================= 算法实现 =================
-    for i in range(size):
-        for j in range(size):
-            if neighborhood[i][j] == 1 and board[i][j] == 0:
-                if utility(board, i, j) < min_value:
-                    row = i
-                    column = j
-                    min_value = utility(board, i, j)
-
-    while board[row][column] != 0:
-        row = random.randint(0, size - 1)
-        column = random.randint(0, size - 1)
-    # ===========================================
-
-    return row, column
-
-
-# 下在i行j列时的效用值
-def utility(board, i, j):
-    board[i][j] = -1
-    board_T = np.transpose(board)
-    board_m = np.flip(board, 0)
-    value = 0
-
-    # 行列扫描
-    for x in range(np.size(board[0])):
-        value += utility_line(board[x])
-        value += utility_line(board_T[x])
-
-    # 对角线扫描
-    for x in range(-np.size(board) + 5, np.size(board) - 4):
-        value += utility_line(board.diagonal(offset=x))
-        value += utility_line(board_m.diagonal(offset=x))
-
-    board[i][j] = 0
-    return value
 
 
 # 评估一条线上的棋形
 def utility_line(line):
+    if type(line) == int or len(line) < 5:
+        return 0
     for entry in value_table:
         if is_subsequence(line, entry[0]):
             return entry[1]
@@ -118,3 +76,106 @@ def is_subsequence(a, b):
         if flag:
             return True
     return False
+
+
+class AI(object):
+
+    # 用于防止数组越界
+    def __limit(self, n):
+        if n < 0:
+            return 0
+        if n >= self.size:
+            return self.size - 1
+        else:
+            return n
+
+    # 更新搜索域，每当落下一颗棋子时，将它周围的8个位置设为搜索域
+    def update_search_area(self, row, column):
+        self.search_area[self.__limit(row - 1)][self.__limit(column - 1)] = \
+            self.search_area[self.__limit(row)][self.__limit(column - 1)] = \
+            self.search_area[self.__limit(row + 1)][self.__limit(column - 1)] = \
+            self.search_area[self.__limit(row - 1)][self.__limit(column)] = \
+            self.search_area[self.__limit(row + 1)][self.__limit(column)] = \
+            self.search_area[self.__limit(row - 1)][self.__limit(column + 1)] = \
+            self.search_area[self.__limit(row)][self.__limit(column + 1)] = \
+            self.search_area[self.__limit(row + 1)][self.__limit(column + 1)] = 1
+
+    # 更新存储的效用值
+    def update_utility(self, row, column):
+        u_r = utility_line(self.board[row])
+        u_c = utility_line(self.board[:, column])
+        # 对角线真烦人
+        u_d1 = utility_line(self.board.diagonal(offset=column - row))
+        mirror = np.fliplr(self.board)
+        u_d2 = utility_line(mirror.diagonal(offset=self.size - row - column - 1))
+
+        self.__utility_sum += u_r - self.__utility_row[row] + u_c - self.__utility_column[column] + \
+            u_d1 - self.__utility_diagonal1[column - row + self.size - 1] + \
+            u_d2 - self.__utility_diagonal2[2 * self.size - row - column - 2]
+
+        self.__utility_row[row] = u_r
+        self.__utility_column[column] = u_c
+        self.__utility_diagonal1[column - row + self.size - 1] = u_d1
+        self.__utility_diagonal2[2 * self.size - row - column - 2] = u_d2
+
+    # 下在i行j列时的效用值
+    def utility(self, player, i, j):
+        self.board[i][j] = player
+        mirror = np.fliplr(self.board)
+        value = self.__utility_sum + \
+            utility_line(self.board[i]) + \
+            utility_line(self.board[:, j]) + \
+            utility_line(self.board.diagonal(offset=j - i)) + \
+            utility_line(mirror.diagonal(offset=self.size - i - j - 1))
+        value -= self.__utility_row[i] + self.__utility_column[j] + \
+            self.__utility_diagonal1[j - i + self.size - 1] + self.__utility_diagonal2[2 * self.size - i - j - 2]
+        self.board[i][j] = 0
+        return value
+
+    # AI接口
+    def ai_move(self):
+        size = np.size(self.board, 0)
+        row = size // 2
+        column = size // 2
+        min_value = 114514
+
+        # ================= 算法实现 =================
+        for i in range(size):
+            for j in range(size):
+                if self.search_area[i][j] == 1 and self.board[i][j] == 0:
+                    utility = self.utility(-1, i, j)
+                    if utility < min_value:
+                        row = i
+                        column = j
+                        min_value = utility
+
+        # 测试用
+        # ===========================================
+        print("=====================================")
+
+        tmp = np.full([self.size, self.size], -1)
+
+        for i in range(size):
+            for j in range(size):
+                if self.search_area[i][j] == 1 and self.board[i][j] == 0:
+                    tmp[i][j] = self.utility(-1, i, j)
+        print(tmp)
+        print("=====================================")
+
+        return row, column
+
+    # 初始化
+    def __init__(self, board):
+        self.board = board
+        self.size = np.size(board[0])
+        self.search_area = np.zeros([self.size, self.size], int)  # 搜索域
+        # 这几个字段的作用是存储每一行，每一列，每一对角线的效用值
+        # 之所以这样做，是因为：每下一步棋只会改变棋子所在的行、列、对角线的效用值
+        # 而之前的算法每次都要重新计算整个棋盘，效率很低
+        # 因此改为分开存储每一行、列、对角线的效用值，每下一步棋后调用 update_utility 更新一次
+        # 这样就大大减少了计算量
+        self.__utility_row = np.zeros([self.size], int)
+        self.__utility_column = np.zeros([self.size])
+        self.__utility_diagonal1 = np.zeros([2 * self.size - 1])
+        self.__utility_diagonal2 = np.zeros([2 * self.size - 1])
+        self.__utility_sum = 0
---
+++ main.py
@@ -0,0 +1,133 @@
+# 图形界面
+import tkinter as tk
+import tkinter.messagebox
+import numpy as np
+import sys
+
+import game_ai
+
+
+class Game(object):
+    # 初始化
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.resizable(width=False, height=False)
+        self.window.title('五子棋')
+
+        self.line_width = 3  # 一根线的宽度
+        self.grid_width = 40  # 一个格子的宽度
+        self.border_width = 40  # 边界的宽度
+        self.size = 15 * self.line_width + 14 * self.grid_width + 2 * self.border_width  # 尺寸
+
+        # 绘制棋盘
+        self.canvas = tk.Canvas(self.window, background='white', width=self.size, height=self.size)
+        self.img_board = tk.PhotoImage(file="images/board.png")
+        self.img_piece_black = tk.PhotoImage(file="images/piece_black.png")
+        self.img_piece_white = tk.PhotoImage(file="images/piece_white.png")
+        self.canvas.create_image(self.size // 2, self.size // 2, image=self.img_board)
+        self.canvas.pack()
+        self.canvas.bind('<Button-1>', self.onclick)
+
+        self.game_board = np.zeros([15, 15])  # 棋盘用numpy数组表示
+        self.ai = game_ai.AI(self.game_board)  # AI初始化
+
+    # 绘制棋子，row和col代表棋子所在的列，player=1代表黑棋，player=-1代表白棋
+    def draw_pieces(self, row, column, player):
+        pos_x = self.border_width + self.line_width // 2 + 1 + (self.grid_width + self.line_width) * column
+        pos_y = self.border_width + self.line_width // 2 + 1 + (self.grid_width + self.line_width) * row
+        if player == 1:  # 黑棋
+            self.canvas.create_image(pos_x, pos_y, image=self.img_piece_black)
+        elif player == -1:  # 白棋
+            self.canvas.create_image(pos_x, pos_y, image=self.img_piece_white)
+
+    # 玩家行棋一步，开始一个回合
+    def round(self, player_row, player_column):
+        self.game_board[player_row][player_column] = 1  # 更新棋盘
+        self.draw_pieces(player_row, player_column, 1)  # 画出玩家的棋子
+        self.window.update()  # 刷新屏幕
+
+        # 胜负判断
+        if self.game_judge() == 1:
+            win_message(1)
+            sys.exit()
+        elif self.game_judge() == -1:
+            win_message(-1)
+            sys.exit()
+
+        self.ai.update_search_area(player_row, player_column)  # 更新AI的搜索域
+        self.ai.update_utility(player_row, player_column)  # 更新AI的效用值
+
+        ai_row, ai_column = self.ai.ai_move()
+        # 确定一下是否下到了合法的位置
+        assert 0 <= ai_row <= 14 and 0 <= ai_column <= 14 and self.game_board[ai_row][ai_column] == 0
+        self.game_board[ai_row][ai_column] = -1  # 更新棋盘
+        self.draw_pieces(ai_row, ai_column, -1)  # 画出AI的棋子
+        self.window.update()  # 刷新屏幕
+
+        # 胜负判断
+        if self.game_judge() == 1:
+            win_message(1)
+            sys.exit()
+        elif self.game_judge() == -1:
+            win_message(-1)
+            sys.exit()
+
+        self.ai.update_search_area(ai_row, ai_column)  # 更新AI的搜索域
+        self.ai.update_utility(ai_row, ai_column)  # 更新AI的效用值
+
+    # 判断游戏胜负，1代表玩家胜利，-1代表电脑胜利，0代表无人胜利
+    def game_judge(self):
+        board_t = self.game_board.transpose()
+        board_mirror = np.flip(self.game_board, 0)
+        # 行与列
+        for i in range(15):
+            for j in range(11):
+                # 5个一行
+                if (self.game_board[i][j: j + 5] == [1, 1, 1, 1, 1]).all():
+                    return 1
+                elif (self.game_board[i][j: j + 5] == [-1, -1, -1, -1, -1]).all():
+                    return -1
+                # 5个一列
+                if (board_t[i][j: j + 5] == [1, 1, 1, 1, 1]).all():
+                    return 1
+                elif (board_t[i][j: j + 5] == [-1, -1, -1, -1, -1]).all():
+                    return -1
+        # 对角线
+        for i in range(11):
+            for j in range(11):
+                if self.game_board[i][j] == 1 and self.game_board[i + 1][j + 1] == 1 and self.game_board[i + 2][j + 2] == 1 \
+                        and self.game_board[i + 3][j + 3] == 1 and self.game_board[i + 4][j + 4] == 1:
+                    return 1
+                elif self.game_board[i][j] == -1 and self.game_board[i + 1][j + 1] == -1 and self.game_board[i + 2][j + 2] == -1 \
+                        and self.game_board[i + 3][j + 3] == -1 and self.game_board[i + 4][j + 4] == -1:
+                    return -1
+                if board_mirror[i][j] == 1 and board_mirror[i + 1][j + 1] == 1 and board_mirror[i + 2][j + 2] == 1 \
+                        and board_mirror[i + 3][j + 3] == 1 and board_mirror[i + 4][j + 4] == 1:
+                    return 1
+                elif board_mirror[i][j] == -1 and board_mirror[i + 1][j + 1] == -1 and board_mirror[i + 2][j + 2] == -1 \
+                        and board_mirror[i + 3][j + 3] == -1 and board_mirror[i + 4][j + 4] == -1:
+                    return -1
+        return 0
+
+    # 鼠标事件
+    def onclick(self, event):
+        column = (event.x - self.border_width // 2) // (self.line_width + self.grid_width)
+        row = (event.y - self.border_width // 2) // (self.line_width + self.grid_width)
+        if 0 <= row <= 14 and 0 <= column <= 14 and self.game_board[row][column] == 0:
+            self.round(row, column)
+
+    # 游戏启动
+    def start(self):
+        self.window.mainloop()
+
+
+def win_message(player):
+    if player == 1:
+        tk.messagebox.showinfo("胜利", "玩家胜利!")
+    elif player == -1:
+        tk.messagebox.showinfo("胜利", "AI胜利！")
+
+
+if __name__ == "__main__":
+    Game = Game()
+    Game.start()
